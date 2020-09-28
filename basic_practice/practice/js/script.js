77c6e0633de759b2abc8bb01f94808af2283d861
木下優希 <yuki04260524@kinoshitayuukinoMacBook-Pro.local>
2020-09-28 16:29:26 +0900

    u

--- basic_practice/practice/js/script.js
+++ basic_practice/practice/js/script.js
@@ -1,13 +1,12 @@
-
 $(function(){
-  
-    $(".btn").on("click",()=>{
+
+  $(".btn").on("click",()=>{
       $("#slide").slideToggle();
-    })
-    $("#slide").slick({
-    autplay: true,
-    autoplaySpeed: 2000,
-    dots:true});
-  
-  
-  });
+  })
+  $("#slide").slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots:true});
+
+
+});
